98dcc55c8ec26ca29060f868539daa995fcf2711
Jonas <jonas.riedel@ionos.com>
2023-07-17 17:14:02 +0200

    Initial commit

---
+++ setup-control-plane.sh
@@ -0,0 +1,31 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y curl apt-transport-https ca-certificates software-properties-common libseccomp2
+
+sudo apt install -y containerd
+
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo add-apt-repository "deb https://apt.kubernetes.io/ kubernetes-xenial main"
+
+sudo apt update
+sudo apt install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo swapoff -a
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+
+sudo modprobe br_netfilter
+echo "br_netfilter" | sudo tee /etc/modules-load.d/br_netfilter.conf
+echo "1" | sudo tee /proc/sys/net/bridge/bridge-nf-call-iptables
+
+echo "net.ipv4.ip_forward = 1" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+
+sudo kubeadm init
+
+curl -O https://docs.projectcalico.org/archive/v3.24/manifests/calico.yaml
+kubectl apply -f calico.yaml
